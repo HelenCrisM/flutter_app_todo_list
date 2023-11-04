d52e882d93634a23935c71b1237ad51f7f614ce6
Helen Cris <helencrisfernandes@hotmail.com>
2023-11-03 23:42:51 -0300

    feature: created task_card_widget

--- lib/ui/widgets/task_card_widget.dart
+++ lib/ui/widgets/task_card_widget.dart
@@ -1,49 +1,62 @@
+import 'dart:developer';
+
 import 'package:flutter/material.dart';
+import 'package:get/get.dart';
 import 'package:todo_list/controllers/task_card_controller.dart';
 
+import '../../controllers/task_controller.dart';
+
+// ignore: must_be_immutable
 class TaskCardWidget extends StatefulWidget {
-  const TaskCardWidget({super.key, required this.title});
+  TaskCardWidget({
+    super.key,
+    required this.title,
+    required this.removeItem,
+    required this.isChecked,
+  });
 
   final String title;
+  final VoidCallback removeItem;
+  bool isChecked;
+
   @override
   State<TaskCardWidget> createState() => _TaskCardWidgetState();
 }
 
 class _TaskCardWidgetState extends State<TaskCardWidget> {
-  bool isChecked = false;
+  TaskCardController controller = Get.put(TaskCardController());
+  TaskController taskController = Get.put(TaskController());
 
   @override
   Widget build(BuildContext context) {
-    return Dismissible(
-      key: UniqueKey(),
-      direction: DismissDirection.endToStart,
-      background: const ColoredBox(
-        color: Colors.red,
-        child: Align(
-          alignment: Alignment.centerRight,
-          child: Padding(
-            padding: EdgeInsets.all(16.0),
-            child: Icon(Icons.delete, color: Colors.white),
-          ),
-        ),
-      ),
+    return Padding(
+      padding: const EdgeInsets.all(6.0),
       child: ListTile(
           leading: Checkbox(
             checkColor: Colors.white,
-            // fillColor: MaterialStateProperty.resolveWih(g),
-            value: isChecked,
+            value: widget.isChecked,
             shape: const CircleBorder(),
             onChanged: (bool? value) {
               setState(() {
-                isChecked = value!;
+                widget.isChecked = value!;
               });
+              log('Fui clicado! ${widget.isChecked}');
+
+              taskController.saveTask();
             },
+          ),
+          trailing: TextButton(
+            onPressed: widget.removeItem,
+            child: const Icon(
+              Icons.delete,
+              color: Colors.red,
+            ),
           ),
           title: Text(
             widget.title,
             style: TextStyle(
-              color: TaskCardController.getTextColor(isChecked),
-              decoration: TaskCardController.getTextDecoration(isChecked),
+              color: controller.getTextColor(widget.isChecked),
+              decoration: controller.getTextDecoration(widget.isChecked),
             ),
           )),
     );
